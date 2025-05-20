818d5b7e6675bcf6474383f95fa120fa01426026
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-05-20 19:48:19 +0700

    Create graphics.py
    added function for drawing time series graph

---
+++ src/utils/graphics.py
@@ -0,0 +1,30 @@
+import torch
+import matplotlib.pyplot as plt
+
+from numpy.typing import ArrayLike
+from matplotlib.axes import Axes
+
+def draw_series(
+    ground_truth: ArrayLike,
+    predictions: torch.Tensor,
+    lag_dur: int = 0,
+    ax: Axes | None = None,
+    xlabel: str ='',
+    ylabel: str = '',
+):
+    if lag_dur == 0 and len(ground_truth) != len(predictions):
+        raise ValueError("Mismatch length between ground_truth and predictions with zero lag...")
+    if ax is None:
+        ax = plt.gca()
+        
+    no_day = len(ground_truth)
+    ax.plot(ground_truth, color='blue', label='Ground truth')
+    ax.plot(
+        list(range(lag_dur, no_day)) if lag_dur != 0 else list(range(len(predictions))),
+        predictions, color='orange', label='Prediction'
+    )
+
+    ax.set_xlabel(xlabel.capitalize())
+    ax.set_ylabel(ylabel.capitalize())
+    ax.legend()
+    return ax
