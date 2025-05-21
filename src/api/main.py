89c3b1b4ec37bdb3040f084b4c545bc647bd6200
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-05-21 18:30:56 +0700

    refactor code

--- src/api/main.py
+++ src/api/main.py
@@ -10,23 +10,18 @@ from data_models import (
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
 
 @app.put("/predict")
-def predict(dates: WeatherSequence) -> WeatherStats:
+def predict(date_sequence: WeatherSequence) -> WeatherStats:
     """
     Predict the weather statistics of the next day given a sequence
         of weather statistics at the specified location.
     """
-    in_features = []
-    for i, date in enumerate(dates):
-        in_features.append([
-            date.humidity, date.pressure, date.temperature,
-            date.wind_direction, date.wind_speed
-        ])
+    in_features = [[
+            stats.humidity, stats.pressure, stats.temperature,
+            stats.wind_direction, stats.wind_speed
+        ] for stats in date_sequence
+    ]
     in_features = torch.tensor(in_features)
     output = in_features.sum(0)
 
