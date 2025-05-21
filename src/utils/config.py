5ff83e3c1442a45a41986b64d39dcddab839decd
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-05-21 19:50:16 +0700

    Update config.py
    add method for converting to dict to save as checkpoints

--- src/utils/config.py
+++ src/utils/config.py
@@ -7,3 +7,12 @@ class Training_Config():
     learning_rate = 1e-5
     epochs = 0
     lag_duration = 7
+
+    def to_dict(self):
+        return {
+            'random_state': self.random_state,
+            'batch_size': self.batch_size,
+            'learning_rate': self.learning_rate,
+            'epochs': self.epochs,
+            'lag_duration': self.lag_duration,
+        }
