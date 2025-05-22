50697c6c183d25d123f0521f58c2859ce55e684f
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-05-22 17:06:06 +0700

    added function for normalizing the values
    with respect to the min and max found in training of course

--- src/api/utils.py
+++ src/api/utils.py
@@ -1,6 +1,12 @@
 import torch
+import pandas as pd
 
 from utils.lstm import LSTMRegressor
+
+
+_temp = pd.read_csv('../resource/dataset/train-test-split/metascale.csv', index_col=0)
+_minimums = torch.tensor(_temp.loc['min'].to_numpy(), dtype=torch.float32)
+_difference = torch.tensor(_temp.loc['max'].to_numpy(), dtype=torch.float32) - _minimums
 
 
 def load_checkpoint(
@@ -16,3 +22,8 @@ def load_checkpoint(
         )['model']
     )
     return model
+
+def normalize_features(features: torch.Tensor) -> torch.Tensor:
+    repeated_min = _minimums.repeat(features.shape[0], 1)
+    repeated_diff = _difference.repeat(features.shape[0], 1)
+    return (features - repeated_min) / repeated_diff
