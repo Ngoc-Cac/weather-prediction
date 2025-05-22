a8cf5ad4714b6547187e9ed706b8374df982cb7e
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-05-22 14:15:15 +0700

    bug fix and enhancement
    - bug fix for model's unrealistic output
    - moved model inference within no_grad block for memory optimization.

--- src/api/main.py
+++ src/api/main.py
@@ -11,14 +11,22 @@ from api.data_models import (
 from utils.lstm import LSTMRegressor
 
 
+device = 'gpu' if torch.cuda.is_available() else 'cpu'
 weather_model = load_checkpoint(
     '../resource/models/lstm_only/4layer_cp3.tar',
     LSTMRegressor(7, 5, num_layers=4, fc_hidden_dims=()),
-    device='gpu' if torch.cuda.is_available() else 'cpu'
+    device=device
 )
+weather_model.eval()
 
 
-app = FastAPI()
+app = FastAPI(
+    version="0.1.0",
+    title="Weather Prediction with LSTM API",
+    summary="",
+    description="",
+    docs_url="/documentation"
+)
 
 
 @app.put("/predict/")
@@ -26,6 +34,9 @@ def predict(date_sequence: WeatherSequence) -> WeatherStats:
     """
     Predict the weather statistics of the next day given a sequence
         of weather statistics at the specified location.
+
+    Unlike the WeatherStats, this will output unbounded results and thus,
+        may be unrealistic.
     """
     in_features = [[
             stats.humidity, stats.pressure, stats.temperature,
@@ -33,12 +44,16 @@ def predict(date_sequence: WeatherSequence) -> WeatherStats:
             *date_sequence.city_coords
         ] for stats in date_sequence
     ]
-    output = weather_model(torch.tensor(in_features).unsqueeze(0)).squeeze()
+    with torch.no_grad():
+        output = weather_model(
+            torch.tensor(in_features, device=device)
+                 .unsqueeze(0)
+        ).squeeze().cpu()
 
-    return WeatherStats(
-        humidity=output[0].item(),
-        pressure=output[1].item(),
-        temperature=output[2].item(),
-        wind_direction=output[3].item(),
-        wind_speed=output[4].item()
-    )
+    return {
+        "humidity" : output[0].item(),
+        "pressure" : output[1].item(),
+        "temperature" : output[2].item(),
+        "wind_direction" : output[3].item(),
+        "wind_speed" : output[4].item()
+    }
