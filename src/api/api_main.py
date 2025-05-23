3094c26bf963e90658755cf214a7b09dd744530a
Phi Yen Nguyen <phiyenng.work@gmail.com>
2025-05-24 02:13:13 +0700

    Update api_main.py

--- src/api/api_main.py
+++ src/api/api_main.py
@@ -32,7 +32,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Cho phép React frontend gọi
+    allow_origins=["http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
